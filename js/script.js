16e68b8268d34cc87fa98e77c96687bd22133ad2
Jeff-desv <brunojeff0012@gmail.com>
2021-09-26 21:13:26 -0300

    calculadora js

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+const calcular = document.getElementById('calcular');
+
+
+
+function imc() {
+    
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if(nome !== '' && altura !== '' && peso !== '') {
+        
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.';
+        } else if(valorIMC < 25) {
+            classificacao = 'No peso ideal. PARABÉNS!!!!!';
+        } else if(valorIMC < 30){
+            classificacao = 'Levemente acima do peso';
+        } else if(valorIMC < 35){
+            classificacao = 'Com obesidade grau I';
+        } else if(valorIMC < 40){
+            classificacao = 'Com obesidade grau II';
+        } else {
+            classificacao = 'Com obesidade grau III. !CUIDADO!'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    } else {
+        resultado.textContent = 'Preencha todos os Campos!!!'
+    }
+
+}
+
+calcular.addEventListener('click', imc);
